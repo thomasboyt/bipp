5a337e9d5f7e31f26f517e6a464ec0a00179cd69
Thomas Boyt <me@thomasboyt.com>
2015-05-30 21:50:26 -0400

    Make measure lines more obvious

--- app/views/lib/Chart/Measure.js
+++ app/views/lib/Chart/Measure.js
@@ -9,13 +9,13 @@ class Measure extends React.Component {
 
     return (
       <svg y={num * this.props.beatSpacing * 4} style={{overflow: 'visible'}}>
-        <line x1="0" x2={WIDTH} style={{strokeWidth: 2}}
+        <line x1="0" x2={WIDTH} style={{strokeWidth: 3}}
           y1={this.props.beatSpacing * 0} y2={this.props.beatSpacing * 0} stroke="black" />
-        <line x1="0" x2={WIDTH}
+        <line x1="0" x2={WIDTH} style={{strokeWidth: 2}}
           y1={this.props.beatSpacing * 1} y2={this.props.beatSpacing * 1} stroke="grey" />
-        <line x1="0" x2={WIDTH}
+        <line x1="0" x2={WIDTH} style={{strokeWidth: 2}}
           y1={this.props.beatSpacing * 2} y2={this.props.beatSpacing * 2} stroke="grey" />
-        <line x1="0" x2={WIDTH}
+        <line x1="0" x2={WIDTH} style={{strokeWidth: 2}}
           y1={this.props.beatSpacing * 3} y2={this.props.beatSpacing * 3} stroke="grey" />
       </svg>
     );
