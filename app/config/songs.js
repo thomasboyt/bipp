192e930e94d6f6e3c8b865923b5788f0d784a937
Thomas Boyt <me@thomasboyt.com>
2015-12-10 23:38:01 -0500

    Remove non-demo songs, gitignore songs/

--- app/config/songs.js
+++ app/config/songs.js
@@ -1,17 +1,5 @@
 export default [
   {
-    'title': 'Keri Baby (feat. Hannah Diamond)',
-    'artist': 'A. G. Cook',
-
-    'data': {
-      'easy': require('../../songs/keri-baby/keri-easy.json'),
-      'hard': require('../../songs/keri-baby/keri.json'),
-    },
-
-    'musicUrl': require('../../songs/keri-baby/keri.mp3'),
-    'youtubeId': 'NoJfEv4eqzs'
-  },
-  {
     'title': 'Click',
     'artist': 'Metronomer',
 
@@ -20,5 +8,5 @@ export default [
     },
 
     'musicUrl': require('../../songs/click/click.mp3')
-  },
+  }
 ];
