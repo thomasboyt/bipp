50c83aab5d8683efbb5ede30d37accbd33014e13
Thomas Boyt <me@thomasboyt.com>
2015-12-17 04:53:40 -0500

    Implement measure/text display in canvas chart, replace css flip w/ canvas

--- app/views/lib/Charts/CanvasChart/index.js
+++ app/views/lib/Charts/CanvasChart/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import I from 'immutable';
+import _ from 'lodash';
+
+import ordinal from '../../../../util/ordinal';
 
 import RenderedCanvas from './RenderedCanvas';
 
@@ -8,9 +11,9 @@ import {LANE_WIDTH, CENTER_LANE_WIDTH, NOTE_HEIGHT, colors, WIDTH} from '../cons
 export {WIDTH};
 export const HEIGHT = 720;
 
-function renderOffsetBar(ctx, {offsetBarY, viewportWidth}) {
+function renderOffsetBar(ctx, {offsetBarY}) {
   ctx.fillStyle = '#4A90E2';
-  ctx.fillRect(0, offsetBarY - (NOTE_HEIGHT / 2), viewportWidth, NOTE_HEIGHT);
+  ctx.fillRect(0, offsetBarY - (NOTE_HEIGHT / 2), WIDTH, NOTE_HEIGHT);
 }
 
 function renderNote(ctx, {note, y}) {
@@ -43,25 +46,71 @@ export function renderNotes(ctx, {notes, offset, beatSpacing, offsetBarY}) {
   });
 }
 
-export function renderChart(ctx, {notes, offset, offsetPositionYPercent, beatSpacing, measures}) {
-  const viewportHeight = ctx.canvas.height;
-  const viewportWidth = ctx.canvas.width;
+function renderBeatLine(ctx, {y, wholeNote}) {
+  ctx.save();
 
-  ctx.fillStyle = '#333';
-  ctx.fillRect(0, 0, viewportWidth, viewportHeight);
+  if (wholeNote) {
+    ctx.lineWidth = 3;
+  } else {
+    ctx.lineWidth = 1;
+  }
+
+  ctx.strokeStyle = 'black';
+  ctx.beginPath();
+  ctx.moveTo(0, y);
+  ctx.lineTo(WIDTH, y);
+  ctx.stroke();
+
+  ctx.restore();
+}
+
+function renderMeasures(ctx, {offset, offsetBarY, beatSpacing, minimumOffset, maximumOffset}) {
+  const beatOffsets = _.range(Math.floor(minimumOffset), Math.ceil(maximumOffset))
+    .filter((val) => val % 24 === 0 && val >= 0);
+
+  beatOffsets.forEach((beatOffset) => {
+    const offsetDiff = beatOffset - offset;
+
+    const y = ((beatSpacing / 24) * offsetDiff) + offsetBarY;
+
+    renderBeatLine(ctx, {y, wholeNote: beatOffset % 96 === 0});
+  });
+}
+
+function renderOffsetText(ctx, {offset, offsetBarY, scrollResolution}) {
+  const noteName = ordinal((24 / scrollResolution) * 4);
+  const beat = Math.floor(offset / 24 / 4) + 1;
+
+  ctx.font = '16px Helvetica';
+  ctx.fillStyle = 'black';
+  ctx.textAlign = 'right';
+
+  ctx.fillText(noteName, WIDTH + 50, HEIGHT - offsetBarY - 5);
+  ctx.fillText(beat, WIDTH + 50, HEIGHT - offsetBarY + 20 - 5);
+}
+
+export function renderChart(ctx, {notes, offset, offsetPositionYPercent, beatSpacing,
+                            showMeasures, showOffsetText, scrollResolution}) {
+  ctx.save();
+  ctx.translate(0, HEIGHT);
+  ctx.scale(1, -1);
 
   const offsetY = offset * (beatSpacing / 24);
 
-  const offsetBarY = (1 - offsetPositionYPercent) * viewportHeight;
+  const offsetBarY = (1 - offsetPositionYPercent) * HEIGHT;
 
-  renderOffsetBar(ctx, {offsetBarY, viewportWidth});
+  renderOffsetBar(ctx, {offsetBarY, WIDTH});
 
   const minimumY = offsetY - offsetBarY - (NOTE_HEIGHT / 2);
-  const maximumY = offsetY + (viewportHeight - offsetBarY) + (NOTE_HEIGHT / 2);
+  const maximumY = offsetY + (HEIGHT - offsetBarY) + (NOTE_HEIGHT / 2);
   const minimumOffset = minimumY / (beatSpacing / 24);
   const maximumOffset = maximumY / (beatSpacing / 24);
 
   const notesToRender = notes.filter((note) => minimumOffset < note.totalOffset && note.totalOffset < maximumOffset);
+
+  if (showMeasures) {
+    renderMeasures(ctx, {offset, offsetBarY, beatSpacing, minimumOffset, maximumOffset});
+  }
 
   renderNotes(ctx, {
     notes: notesToRender,
@@ -70,7 +119,11 @@ export function renderChart(ctx, {notes, offset, offsetPositionYPercent, beatSpa
     beatSpacing,
   });
 
-  // TODO: Render measures
+  ctx.restore();
+
+  if (showOffsetText) {
+    renderOffsetText(ctx, {offset, offsetBarY, scrollResolution});
+  }
 }
 
 const CanvasChart = React.createClass({
@@ -84,13 +137,22 @@ const CanvasChart = React.createClass({
     offsetPositionYPercent: React.PropTypes.number.isRequired,
     beatSpacing: React.PropTypes.number.isRequired,
     numMeasures: React.PropTypes.number.isRequired,
+
+    // Editor stuff
+    showMeasures: React.PropTypes.bool,
+    showOffsetText: React.PropTypes.bool,
+    scrollResolution: React.PropTypes.number,
   },
 
   render() {
+    // Add padding if we're displaying right-hand chart information in editor
+    // (we don't want to do this in player b/c it'll mess up centering)
+    const width = this.props.showOffsetText ? WIDTH + 100 : WIDTH;
+
     return (
       <div className="chart-overflower">
         <RenderedCanvas render={renderChart} params={this.props}
-          height={HEIGHT} width={WIDTH} style={{transform: 'scale(1, -1)'}} />
+          height={HEIGHT} width={width} />
       </div>
     );
   }
