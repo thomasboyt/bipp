a3f15f5a628721a72d3c6130c202bdc9c7c18ebc
Thomas Boyt <thomas@ledgerx.com>
2016-01-07 12:47:31 -0500

    Clean up playback spec

--- app/reducers/__tests__/playback.spec.js
+++ app/reducers/__tests__/playback.spec.js
@@ -24,48 +24,45 @@ const chart = chartReducer(undefined, {
 });
 
 describe('playback reducer', () => {
+  const initState = reducer(undefined, {
+    type: ENTER_PLAYBACK,
+    offset: 0,
+    bpm: chart.bpm,
+    notes: chart.notes,
+    beatSpacing: 0,
+  });
 
-  describe('playing notes', () => {
-    const initState = reducer(undefined, {
-      type: ENTER_PLAYBACK,
-      offset: 0,
-      bpm: chart.bpm,
-      notes: chart.notes,
-      beatSpacing: 0,
+  // time of first note, which is at offset = 0
+  // TODO: this could be some kind of util method, getTimeFor(offset)
+  const initTime = initState.startTime - initState.initialOffsetMs;
+
+  it('playing a note increments combo', () => {
+    const playedState = reducer(initState, {
+      type: PLAY_NOTE,
+      time: initTime,
+      column: 3,
     });
 
-    // time of first note, which is at offset = 0
-    // TODO: this could be some kind of util method, getTimeFor(offset)
-    const initTime = initState.startTime - initState.initialOffsetMs;
+    expect(playedState.combo).toEqual(1);
+    expect(playedState.maxCombo).toEqual(1);
+  });
 
-    it('playing a note increments combo', () => {
-      const playedState = reducer(initState, {
-        type: PLAY_NOTE,
-        time: initTime,
-        column: 3,
-      });
-
-      expect(playedState.combo).toEqual(1);
-      expect(playedState.maxCombo).toEqual(1);
+  it('missing a note resets combo', () => {
+    const playedState = reducer(initState, {
+      type: PLAY_NOTE,
+      time: initTime,
+      column: 3,
     });
 
-    it('missing a note resets combo', () => {
-      const playedState = reducer(initState, {
-        type: PLAY_NOTE,
-        time: initTime,
-        column: 3,
-      });
+    expect(playedState.combo).toEqual(1);
 
-      expect(playedState.combo).toEqual(1);
-
-      // tick past the second note's threshold window to allow sweep
-      const missedState = reducer(playedState, {
-        type: TICK,
-        dt: -playedState.initialOffsetMs + (playedState.msPerOffset * 24) + maxJudgementThreshold + 1,
-      });
-
-      expect(missedState.combo).toEqual(0);
-      expect(playedState.maxCombo).toEqual(1);
+    // tick past the second note's threshold window to allow sweep
+    const missedState = reducer(playedState, {
+      type: TICK,
+      dt: -playedState.initialOffsetMs + (playedState.msPerOffset * 24) + maxJudgementThreshold + 1,
     });
+
+    expect(missedState.combo).toEqual(0);
+    expect(playedState.maxCombo).toEqual(1);
   });
 });
