35cf2086243fad2e10a575dea91f05a4201840ef
Thomas Boyt <me@thomasboyt.com>
2016-01-08 22:11:56 -0500

    Document immutable reducer util

--- app/util/immutableReducer.js
+++ app/util/immutableReducer.js
@@ -1,4 +1,15 @@
-export default function creatImmutableReducer(initialState, handlers) {
+/*
+ * lets you define a reducer as a series of action handlers that return new state
+ *
+ * example:
+ *
+ * const reducer = createImmutableReducer(initialState, {
+ *   [ACTION_TYPE]: function(action, state) {
+ *     return state.set('field', action.field);
+ *   }
+ * });
+ */
+export default function createImmutableReducer(initialState, handlers) {
   return (state = initialState, action) => {
     if (handlers[action.type]) {
       const newState = handlers[action.type](action, state);
